3f8da74e9c2ec2127c7f24004d85a1fc3fee3f25
chenkaiguang <ckaiguang@163.com>
2025-06-12 00:39:12 +0800

    feat: 支持设置token过期时间

--- devops/makefile_cicd.mk
+++ devops/makefile_cicd.mk
@@ -25,11 +25,11 @@ build-service-image:
 build:
 	@echo "build :-->: build service image"
 	#docker build -t account-service:v1.0.0 -f ./devops/Dockerfile .
-	#docker pull golang:1.22.8
-	#docker pull debian:stable-20240926-slim
+	#docker pull golang:1.23.10
+	#docker pull debian:stable-20250520
 	docker build \
-		--build-arg BUILD_FROM_IMAGE=golang:1.22.8 \
-		--build-arg RUN_SERVICE_IMAGE=debian:stable-20240926-slim \
+		--build-arg BUILD_FROM_IMAGE=golang:1.23.10 \
+		--build-arg RUN_SERVICE_IMAGE=debian:stable-20250520 \
 		--build-arg APP_DIR=app \
 		--build-arg SERVICE_NAME=account-service \
 		--build-arg VERSION=latest \
